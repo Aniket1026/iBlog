e0002fd090699e24d2623a139678f02831389f1c
Aniket <annudubey1026@gmail.com>
2023-03-15 17:31:59 +0530

    refreshToken generated

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,33 +1,48 @@
 import User from "../model/user.js";
-import bcrypt from 'bcrypt'
-import user from "../model/user.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const signupUser = async (request, response) => {
   try {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(request.body.password, salt);
-    const user = {name:request.body.name,username:request.body.username,password:hashedPassword};
+    const user = {
+      name: request.body.name,
+      username: request.body.username,
+      password: hashedPassword,
+    };
     const newUser = new User(user);
     await newUser.save();
     return response.status(200).json({ msg: "SignUp has been successful" });
   } catch (error) {
-    return response.status(500).json({ msg: "Error while signup the user" + error});
+    return response
+      .status(500)
+      .json({ msg: "Error while signup the user" + error });
   }
 };
 
 export const loginUser = async (request, response) => {
-  let user = await User.findOne({ username: request.body.username })
+  let user = await User.findOne({ username: request.body.username });
   if (!user) {
-    return response.status(400).json({ msg: 'Username does not match' })
+    return response.status(400).json({ msg: "Username does not match" });
   }
   try {
     let matchPassword = bcrypt.compare(request.body.password, user.password);
     if (matchPassword) {
-      
+      const accessToken = jwt.sign(
+        user.toJson(),
+        process.env.ACCESS_SECRET_KEY,
+        { expiresIn: "15m" }
+      );
+      const refreshToken = jwt.sign(
+        user.toJson(),
+        process.env.REFRESH_SECRET_KEY
+      );
     } else {
-      return response.status(400).json({msg:'Password does not match'})
+      return response.status(400).json({ msg: "Password does not match" });
     }
-  } catch (error) {
-    
-  }
-}
+  } catch (error) {}
+};
