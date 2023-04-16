4dce2de325cfd16819f3863391bbb8a8c7e2c555
Aniket <annudubey1026@gmail.com>
2023-04-16 17:49:20 +0530

    Forget password component customized

--- iblog/src/components/accounts/ForgotPassword.jsx
+++ iblog/src/components/accounts/ForgotPassword.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
+import React from "react";
 
+import { Box, TextField, Button, styled } from "@mui/material";
+
+const Component = styled(Box)`
+  width: 400px;
+  height: 350px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px;
+`;
+
+const Image = styled("img")({
+  width: 100,
+  height: 60,
+});
 
 const ForgotPassword = () => {
-  return (
-    <div>
-          Password reset
-    </div>
-  )
-}
+  const imageURL =
+    "https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png";
 
-export default ForgotPassword
+  return (
+    <>
+      <Component>
+        <Image src={imageURL} alt="login" />
+        <TextField
+          id="outlined-basic"
+          name="new password"
+          label="new passsword"
+          variant="outlined"
+        />
+        <TextField
+          id="outlined-basic"
+          name="confirm new password"
+          label="confirm new password"
+          variant="outlined"
+        />
+
+        <Button variant="contained">RESET PASSWORD</Button>
+      </Component>
+    </>
+  );
+};
+
+export default ForgotPassword;
